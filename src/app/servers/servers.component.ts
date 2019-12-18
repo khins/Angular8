e6dbcf77916f7bf46006f397fe48e6d5005327bb
kevin <kevin@hinshawfam.com>
2019-12-17 20:10:17 -0600

    understanding component selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   template: `<app-server></app-server>
               testing using back tics
             <app-server></app-server>`,
