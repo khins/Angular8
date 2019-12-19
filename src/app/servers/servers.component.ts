e2651a54cdf48951fb23afaba33bdcc30b3a01b2
kevin <kevin@hinshawfam.com>
2019-12-19 05:25:08 -0600

    apply CSS classes dynamically with ngClass.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '.app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styles: [`
+      .online {
+        color: white
+      }
+  `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
